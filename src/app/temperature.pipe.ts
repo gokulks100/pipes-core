9054132c789bb2bef485e256ba0d239fa6cab277
gokulks <gsuresh@grepruby.io>
2025-03-12 18:10:26 +0530

    pipes core changes added 2 params to inputType and outputType

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -1,23 +1,43 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { input, Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'temp',
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: string|number, ...args: any[]) {
+  transform(
+    value: string | number | null ,
+    inputType: 'cel' | 'fah',
+    outputType: 'cel' | 'fah'
+  ) {
+
+    if(!value) return null
+
     let val: number;
-    if(typeof value === 'string')
-    {
-      val = parseFloat(value)
-    }
-    else
-    {
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
       val = value;
     }
 
-    const outputTemp =  val * (9/5)  + 32;
+    let outputTemp: number;
 
-    return `${outputTemp} F`
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: 'C' | 'F'
+
+    if(!outputType){
+      symbol = inputType ===  'cel'  ? "C" : "F"
+    }else{
+      symbol = outputType ===  'cel'  ? "C" : "F"
+    }
+
+    return `${outputTemp} ${symbol}`;
   }
 }
